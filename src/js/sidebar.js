855dad0cd0819a6cd79c6e1f0cd058becaffef7d
CatarinaEudoxia <ceudoxia.ferreira@gmail.com>
2024-07-04 22:40:53 -0300

    Arrumando responsividade dos itens de acordo com as telas

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,28 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var menuIcon = document.querySelector('.menu-icon');
-    var sidebar = document.getElementById('sidebar');
+    const menuIcon = document.querySelector('.menu-icon');
+    const sidebar = document.getElementById('sidebar');
 
     if (menuIcon) {
         menuIcon.addEventListener('click', function () {
             sidebar.classList.toggle('active');
+            toggleMenuIcon();
         });
     }
 
+    // Função para alternar o ícone do menu
+    function toggleMenuIcon() {
+        const menuIconImg = document.querySelector('.menu-icon-img');
+        const isSidebarActive = sidebar.classList.contains('active');
+
+        // Alterna o ícone com base no estado da barra lateral
+        if (isSidebarActive) {
+            menuIconImg.src = './src/assets/close-icon.svg'; // Ícone de fechar
+        } else {
+            menuIconImg.src = './src/assets/menu-nav.svg'; // Ícone do menu
+        }
+    }
+
     // Fechar a barra lateral ao clicar em um link do sidebar
-    var sidebarLinks = document.querySelectorAll('.sidebar ul li a');
+    const sidebarLinks = document.querySelectorAll('.sidebar ul li a');
     sidebarLinks.forEach(function (link) {
         link.addEventListener('click', function () {
             sidebar.classList.remove('active');
+            toggleMenuIcon();
         });
-    });
-
-    // Fechar a barra lateral ao clicar fora dela (opcional)
-    document.addEventListener('click', function (event) {
-        var isClickInside = sidebar.contains(event.target);
-        var isMenuIcon = event.target.closest('.menu-icon');
-
-        if (!isClickInside && !isMenuIcon) {
-            sidebar.classList.remove('active');
-        }
     });
 });
